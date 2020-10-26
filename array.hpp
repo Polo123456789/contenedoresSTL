c642258a726883c6788ac473bbe10864f38e66a9
polo123456789 <pabsan02@gmail.com>
2020-10-26 17:53:27 -0600

    Ya estamos para entrar al vector

--- array.hpp
+++ array.hpp
@@ -1,6 +1,8 @@
 #ifndef PSG_ARRAY_HPP
 #define PSG_ARRAY_HPP
 
+#include <cstddef>
+
 #include "utility.hpp"
 #include "algorithm.hpp"
 #include "exception.hpp"
@@ -8,40 +10,60 @@
 namespace psg {
 
 template<typename T, const size_t arr_size>
-class array { //NOLINT No necesita los que son por movimiento
+class array { // NOLINT No necesita los que son por movimiento
    public:
+    using value_type = T;
+    using pointer = T *;
+    using const_pointer = const T *;
+    using reference = T &;
+    using const_reference = const T &;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
+
     array(void) = default;
     array(const array<T, arr_size> &rhs);
     array &operator=(const array<T, arr_size> &rhs);
 
-    T &at(int position);
-    T &operator[](int position);
-    T &front(void);
-    T &back(void);
+    reference at(int position);
+    const_reference at(int position) const;
+    reference operator[](int position);
+    const_reference operator[](int position) const;
+    reference front(void);
+    const_reference front(void) const;
+    reference back(void);
+    const_reference back(void) const;
 
     class iterator;
     class const_iterator;
     class reverse_iterator;
     class const_reverse_iterator;
-    iterator begin(void);
-    iterator end(void);
-    const_iterator cbegin(void) const;
-    const_iterator cend(void) const;
-    reverse_iterator rbegin(void);
-    reverse_iterator rend(void);
-    const_reverse_iterator crbegin(void) const;
-    const_reverse_iterator crend(void) const;
+    iterator begin(void) noexcept;
+    iterator end(void) noexcept;
+    const_iterator cbegin(void) const noexcept;
+    const_iterator cend(void) const noexcept;
+    reverse_iterator rbegin(void) noexcept;
+    reverse_iterator rend(void) noexcept;
+    const_reverse_iterator crbegin(void) const noexcept;
+    const_reverse_iterator crend(void) const noexcept;
 
-    [[nodiscard]] constexpr bool empty(void) const;
-    constexpr size_t size(void) const;
-    constexpr size_t max_size(void) const;
+    [[nodiscard]] constexpr bool empty(void) const noexcept;
+    constexpr size_t size(void) const noexcept;
+    constexpr size_t max_size(void) const noexcept;
 
     void fill(const T &value);
     void swap(array<T, arr_size> &other) noexcept;
 
+    pointer data() noexcept;
+    const_pointer data() const noexcept;
+
    private:
-    T data[arr_size]; // NOLINT Tiene que ser un array estilo C
+    T object[arr_size]; // NOLINT Tiene que ser un array estilo C
 };
+
+template<typename T, const size_t arr_size>
+void swap(array<T, arr_size>& a, array<T, arr_size>& b) noexcept {
+    a.swap(b);
+}
 
 template<typename T, const size_t arr_size>
 array<T, arr_size>::array(const array<T, arr_size> &rhs) {
@@ -49,7 +71,7 @@ array<T, arr_size>::array(const array<T, arr_size> &rhs) {
 }
 
 template<typename T, const size_t arr_size>
-array<T, arr_size> &array<T, arr_size>::operator=( //NOLINT Si lo maneja
+array<T, arr_size> &array<T, arr_size>::operator=( // NOLINT Si lo maneja
     const array<T, arr_size> &rhs) {
 
     if (this == &rhs) {
@@ -68,7 +90,22 @@ array<T, arr_size> &array<T, arr_size>::operator=( //NOLINT Si lo maneja
 template<typename T, const size_t arr_size>
 T &array<T, arr_size>::at(int position) {
     if (position >= 0 && position < arr_size) {
-        return data[position];
+        return object[position];
+    }
+    throw exception("exception en el psg::array al llamar la funcion at(), "
+                    "elemento fuera de rango. ");
+}
+
+/* Accede a un elemento del array. Verifica si la posición esta dentro del
+ * array. Si no lo esta lanza un psg::exception
+ *
+ * Parametros:
+ *  - Posición: Posicion a la que se quiere acceder
+ */
+template<typename T, const size_t arr_size>
+const T &array<T, arr_size>::at(int position) const {
+    if (position >= 0 && position < arr_size) {
+        return object[position];
     }
     throw exception("exception en el psg::array al llamar la funcion at(), "
                     "elemento fuera de rango. ");
@@ -76,7 +113,12 @@ T &array<T, arr_size>::at(int position) {
 
 template<typename T, const size_t arr_size>
 T &array<T, arr_size>::operator[](int position) {
-    return data[position];
+    return object[position];
+}
+
+template<typename T, const size_t arr_size>
+const T &array<T, arr_size>::operator[](int position) const {
+    return object[position];
 }
 
 /*
@@ -84,7 +126,15 @@ T &array<T, arr_size>::operator[](int position) {
  */
 template<typename T, const size_t arr_size>
 T &array<T, arr_size>::front(void) {
-    return data[0];
+    return object[0];
+}
+
+/*
+ * Accede al primer elemento del array
+ */
+template<typename T, const size_t arr_size>
+const T &array<T, arr_size>::front(void) const {
+    return object[0];
 }
 
 /*
@@ -92,7 +142,15 @@ T &array<T, arr_size>::front(void) {
  */
 template<typename T, const size_t arr_size>
 T &array<T, arr_size>::back(void) {
-    return data[arr_size - 1];
+    return object[arr_size - 1];
+}
+
+/*
+ * Accede al ultimo elemento del array
+ */
+template<typename T, const size_t arr_size>
+const T &array<T, arr_size>::back(void) const {
+    return object[arr_size - 1];
 }
 
 template<typename T, const size_t arr_size>
@@ -107,40 +165,40 @@ using ArrayRevIter = typename array<T, arr_size>::reverse_iterator;
 template<typename T, const size_t arr_size>
 using ArrayConstRevIter = typename array<T, arr_size>::const_reverse_iterator;
 
-// Regresa un iterador al principio del array 
+// Regresa un iterador al principio del array
 template<typename T, const size_t arr_size>
-ArrayIter<T, arr_size> array<T, arr_size>::begin(void) {
-    return ArrayIter<T, arr_size>(data, 0);
+ArrayIter<T, arr_size> array<T, arr_size>::begin(void) noexcept {
+    return ArrayIter<T, arr_size>(object, 0);
 }
 
 // Regresa un iterador pasado el final del array. NO SE DEBE ACCEDER AL ITERADOR
 template<typename T, const size_t arr_size>
-ArrayIter<T, arr_size> array<T, arr_size>::end(void) {
-    return ArrayIter<T, arr_size>(data, arr_size);
+ArrayIter<T, arr_size> array<T, arr_size>::end(void) noexcept {
+    return ArrayIter<T, arr_size>(object, arr_size);
 }
 
 /* Regresa un iterador al principio del array. Los valores dereferenciados no
  * pueden ser modificados.
  */
 template<typename T, const size_t arr_size>
-ArrayConstIter<T, arr_size> array<T, arr_size>::cbegin(void) const {
-    return ArrayConstIter<T, arr_size>(data, 0);
+ArrayConstIter<T, arr_size> array<T, arr_size>::cbegin(void) const noexcept {
+    return ArrayConstIter<T, arr_size>(object, 0);
 }
 
 /* Regresa un iterador pasado el final del array. Los valores dereferenciados no
  * pueden ser modificados.
  */
 template<typename T, const size_t arr_size>
-ArrayConstIter<T, arr_size> array<T, arr_size>::cend(void) const {
-    return ArrayConstIter<T, arr_size>(data, arr_size);
+ArrayConstIter<T, arr_size> array<T, arr_size>::cend(void) const noexcept {
+    return ArrayConstIter<T, arr_size>(object, arr_size);
 }
 
 /* Regresa un iterador al final del array. Este lo recorrera de el final hacia
  * el inicio.
  */
 template<typename T, const size_t arr_size>
-ArrayRevIter<T, arr_size> array<T, arr_size>::rbegin(void) {
-    T *last_element = data + arr_size - 1;
+ArrayRevIter<T, arr_size> array<T, arr_size>::rbegin(void) noexcept {
+    T *last_element = object + arr_size - 1;
     return ArrayRevIter<T, arr_size>(last_element, 0);
 }
 
@@ -148,8 +206,8 @@ ArrayRevIter<T, arr_size> array<T, arr_size>::rbegin(void) {
  * ITERADOR
  */
 template<typename T, const size_t arr_size>
-ArrayRevIter<T, arr_size> array<T, arr_size>::rend(void) {
-    T *last_element = data + arr_size - 1;
+ArrayRevIter<T, arr_size> array<T, arr_size>::rend(void) noexcept {
+    T *last_element = object + arr_size - 1;
     return ArrayRevIter<T, arr_size>(last_element, arr_size);
 }
 
@@ -157,8 +215,9 @@ ArrayRevIter<T, arr_size> array<T, arr_size>::rend(void) {
  * final hacia el inicio.
  */
 template<typename T, const size_t arr_size>
-ArrayConstRevIter<T, arr_size> array<T, arr_size>::crbegin(void) const {
-    const T *last_element = data + arr_size - 1;
+ArrayConstRevIter<T, arr_size> array<T, arr_size>::crbegin(
+    void) const noexcept {
+    const T *last_element = object + arr_size - 1;
     return ArrayConstRevIter<T, arr_size>(last_element, 0);
 }
 
@@ -166,26 +225,26 @@ ArrayConstRevIter<T, arr_size> array<T, arr_size>::crbegin(void) const {
  * AL ITERADOR
  */
 template<typename T, const size_t arr_size>
-ArrayConstRevIter<T, arr_size> array<T, arr_size>::crend(void) const {
-    const T *last_element = data + arr_size - 1;
+ArrayConstRevIter<T, arr_size> array<T, arr_size>::crend(void) const noexcept {
+    const T *last_element = object + arr_size - 1;
     return ArrayConstRevIter<T, arr_size>(last_element, arr_size);
 }
 
 // Indica si el array esta vacio. Unicamente verdadero cuando el tamaño es 0
 template<typename T, const size_t arr_size>
-[[nodiscard]] constexpr bool array<T, arr_size>::empty(void) const {
+[[nodiscard]] constexpr bool array<T, arr_size>::empty(void) const noexcept {
     return arr_size == 0;
 }
 
 // Regresa el tamaño del array
 template<typename T, const size_t arr_size>
-constexpr size_t array<T, arr_size>::size(void) const {
+constexpr size_t array<T, arr_size>::size(void) const noexcept {
     return arr_size;
 }
 
 // Regresa el tamaño del array
 template<typename T, const size_t arr_size>
-constexpr size_t array<T, arr_size>::max_size(void) const {
+constexpr size_t array<T, arr_size>::max_size(void) const noexcept {
     return arr_size;
 }
 
@@ -207,11 +266,21 @@ void array<T, arr_size>::swap(array<T, arr_size> &other) noexcept {
 }
 
 template<typename T, const size_t arr_size>
+T *array<T, arr_size>::data() noexcept {
+    return object;
+}
+
+template<typename T, const size_t arr_size>
+const T *array<T, arr_size>::data() const noexcept {
+    return object;
+}
+
+template<typename T, const size_t arr_size>
 class array<T, arr_size>::iterator {
    public:
     iterator() = default;
-    explicit iterator(T * first, int position);
-    explicit iterator(T * pos);
+    explicit iterator(T *first, int position);
+    explicit iterator(T *pos);
     iterator &operator--(void);
     iterator operator--(int);
     iterator &operator++(void);
@@ -352,8 +421,8 @@ template<typename T, const size_t arr_size>
 class array<T, arr_size>::const_iterator {
    public:
     const_iterator() = default;
-    explicit const_iterator(const T * first, int position);
-    explicit const_iterator(const T * pos);
+    explicit const_iterator(const T *first, int position);
+    explicit const_iterator(const T *pos);
     const_iterator &operator--(void);
     const_iterator operator--(int);
     const_iterator &operator++(void);
--- test.cpp
+++ test.cpp
@@ -1,1 +1,19 @@
-#include <array>
+#include "array.hpp"
+
+
+int main (void) {
+    const int size = 10;
+    psg::array<int, size> hola{};
+    psg::array<int, size> adios{};
+
+    adios.fill(1);
+
+    hola[1] = 1;
+    hola.back() = 2;
+    hola.front() = 3;
+    hola.at(4) = 4;
+
+    psg::swap(hola, adios);
+    return 0;
+}
+
