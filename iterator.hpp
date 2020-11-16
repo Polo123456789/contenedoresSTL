39a35ddf2fa8cbc18bc55f2767bf1cf70c176ddc
polo123456789 <pabsan02@gmail.com>
2020-11-15 19:41:18 -0600

    Funcion distance.

--- iterator.hpp
+++ iterator.hpp
@@ -94,6 +94,38 @@ struct iterator_traits {
     using iterator_category = typename Iter::iterator_category;
 };
 
+namespace imp {
+
+template<typename InputIt>
+typename iterator_traits<InputIt>::difference_type distance(InputIt first,
+    InputIt last,
+    random_acces_iterator_tag /*unused*/) {
+
+    return last - first;
+}
+
+template<typename InputIt>
+typename iterator_traits<InputIt>::difference_type
+    distance(InputIt first, InputIt last, input_iterator_tag /*unused*/) {
+
+    typename iterator_traits<InputIt>::difference_type d = 0;
+    while (*(first++) != last) {
+        ++d;
+    }
+    return d;
+
+}
+
+}; // namespace imp
+
+template<typename InputIt>
+typename iterator_traits<InputIt>::difference_type distance(InputIt first,
+    InputIt last) {
+    return imp::distance(first,
+        last,
+        typename iterator_traits<InputIt>::iterator_category());
+}
+
 }; // namespace psg
 
 #endif
