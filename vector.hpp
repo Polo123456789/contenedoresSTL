e4238e3e4abe67786c97caf3fe94f14e1d9c9c17
polo123456789 <pabsan02@gmail.com>
2020-11-15 19:08:27 -0600

    Ya tengo el begin y el end para el vector
    Ya con eso avanzamos a las demas partes sin perder tiempo. Ni repetir
    codigo

--- vector.hpp
+++ vector.hpp
@@ -137,24 +137,46 @@ class vector {
 };
 
 namespace imp {
-    template <typename T>
-    using VecIter = iterators::LegacyRandomAccesIterator<T>;
-}
+template<typename T>
+using VecIter = iterators::LegacyRandomAccesIterator<T>;
 
+template<typename T>
+using VecRevIter = iterators::LegacyRandomAccesIterator<T, psg::minus<T>>;
+
+template<typename T>
+using VecConstIter = iterators::constant::LegacyRandomAccesIterator<T>;
+
+template<typename T>
+using VecConstRevIter =
+    iterators::constant::LegacyRandomAccesIterator<T, psg::minus<T>>;
+
+} // namespace imp
+
+/// Iterador al principio del contenedor.
 template<class T, class Allocator>
 typename vector<T, Allocator>::iterator vector<T, Allocator>::begin() noexcept {
+    return imp::VecIter<T>(object, 0);
+}
+
+/// Iterador constante al principio del contenedor.
+template<class T, class Allocator>
+typename vector<T, Allocator>::const_iterator
+    vector<T, Allocator>::begin() const noexcept {
+
+    return imp::VecConstIter<T>(object, 0);
+}
+
+template<class T, class Allocator>
+typename vector<T, Allocator>::iterator vector<T, Allocator>::end() noexcept {
+    return imp::VecIter<T>(object, last_valid_element);
 }
 
 template<class T, class Allocator>
 typename vector<T, Allocator>::const_iterator
-    vector<T, Allocator>::begin() const noexcept {}
+    vector<T, Allocator>::end() const noexcept {
 
-template<class T, class Allocator>
-typename vector<T, Allocator>::iterator vector<T, Allocator>::end() noexcept {}
-
-template<class T, class Allocator>
-typename vector<T, Allocator>::const_iterator
-    vector<T, Allocator>::end() const noexcept {}
+    return imp::VecConstIter<T>(object, last_valid_element);
+}
 
 /// Simplemente asigna el allocator.
 template<class T, class Allocator>
