d56b6f76a4bdfbdb6bdd3737b6d9f99a3906790b
polo123456789 <pabsan02@gmail.com>
2020-12-06 14:42:35 -0600

    (+)<generate_bin> Instrucciones para usar VS

--- tests/generate_bin.bat
+++ tests/generate_bin.bat
@@ -1,4 +1,9 @@
 @echo off
+
+REM    Nesecita llamar a vcvars64.bat para correrlo. Lo comilas con nmake
+REM    You need to call vcvars64.bat to run it for visual studio. You run it
+REM    with nmake
+REM    @call %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 rmdir bin_msv /s /q
 mkdir bin_msv
 cd bin_msv
