46168e8318a323a33d7cc548adbe3e508cbf606f
polo123456789 <pabsan02@gmail.com>
2020-12-07 13:22:16 -0600

    (+)<iterator> Reverse iterator dependiendo del tipo

--- include/psg/iterator/reverse_iterator.hpp
+++ include/psg/iterator/reverse_iterator.hpp
@@ -4,9 +4,28 @@
 #include <psg/iterator/reverse_iterator/bidirectional_iterator.hpp>
 #include <psg/iterator/reverse_iterator/random_access_iterator.hpp>
 
+#include <psg/iterator/iterator_tags.hpp>
+#include <psg/iterator/iterator_traits.hpp>
+
+#include <psg/type_traits.hpp>
+
 namespace psg {
-//    template <typename Iter>
-        //using reverse_iterator = 
-}
+
+/// Adaptador para que cualquier iterador, ya sea bidirectional o de random
+/// access pueda utilizarse como iterador reverso
+template<typename Iter>
+using reverse_iterator =
+    // clang-format off
+    conditional_t<
+        is_same_v<
+            typename iterator_traits<Iter>::iterator_category,
+            bidirectional_iterator_tag
+        >,
+        imp::reverce_bidirecitional_iterator<Iter>,
+        imp::reverse_random_access_iterator<Iter>
+    >;
+// clang-format on
+
+}; // namespace psg
 
 #endif
