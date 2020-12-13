d58868ce210381ecf0aa96dbee4568b18a2fc8ea
polo123456789 <pabsan02@gmail.com>
2020-12-13 15:17:02 -0600

    (~)<utility>Inclucion redudante de header eliminada.

--- include/psg/utility.hpp
+++ include/psg/utility.hpp
@@ -1,7 +1,6 @@
 #ifndef PSG_UTILITY_HPP
 #define PSG_UTILITY_HPP
 
-#include <psg/type_traits/reference_modifications.hpp>
 #include <psg/utility/functions.hpp>
 
 namespace psg {}; // namespace psg
