bade4258c43f8ccce0f82a5260dbb9bcd52b3b30
polo123456789 <pabsan02@gmail.com>
2020-12-13 08:08:47 -0600

    (~)<allocator_traits>Tipos largos pasan a usar trailing return type

--- tests/generate_bin.bat
+++ tests/generate_bin.bat
@@ -3,7 +3,7 @@
 REM    Nesecita llamar a vcvars64.bat para correrlo. Lo comilas con nmake
 REM    You need to call vcvars64.bat to run it for visual studio. You run it
 REM    with nmake
-REM    @call %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+REM @call %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 rmdir bin_msv /s /q
 mkdir bin_msv
 cd bin_msv
@@ -14,7 +14,7 @@ REM Ver compile_all.bat
 rmdir bin_gcc /s /q
 mkdir bin_gcc
 cd bin_gcc
-cmake .. -G "Unix Makefiles"
+cmake .. -G "Unix Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
 cd ..
 
 rmdir bin_clang /s /q
