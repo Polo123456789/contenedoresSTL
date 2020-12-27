068ff14050b851486670e5f733f8de51c13330d6
polo123456789 <pabsan02@gmail.com>
2020-12-27 15:50:53 -0600

    (+)<type_traits> add lvalue rvalue reference con tests

--- include/psg/type_traits/reference_modifications.hpp
+++ include/psg/type_traits/reference_modifications.hpp
@@ -34,12 +34,35 @@ struct remove_reference<T &&> {
 template<typename T>
 using remove_reference_t = typename remove_reference<T>::type;
 
+namespace imp {
+
+// https://en.cppreference.com/w/cpp/types/add_reference
 template<class T>
-struct add_lvalue_reference;
+struct type_identity {
+    using type = T;
+};
+
 template<class T>
-struct add_rvalue_reference;
+auto try_add_lvalue_reference(int) -> type_identity<T &>;
+template<class T>
+auto try_add_lvalue_reference(...) -> type_identity<T>;
+
+template<class T>
+auto try_add_rvalue_reference(int) -> type_identity<T &&>;
+template<class T>
+auto try_add_rvalue_reference(...) -> type_identity<T>;
+
+}; // namespace imp
+
+template<class T>
+struct add_lvalue_reference : decltype(imp::try_add_lvalue_reference<T>(0)) {};
+
 template<class T>
 using add_lvalue_reference_t = typename add_lvalue_reference<T>::type;
+
+template<class T>
+struct add_rvalue_reference : decltype(imp::try_add_rvalue_reference<T>(0)) {};
+
 template<class T>
 using add_rvalue_reference_t = typename add_rvalue_reference<T>::type;
 
---
+++ tests/type_traits.cpp
@@ -0,0 +1,44 @@
+#include <psg/type_traits.hpp>
+
+void type_traits_assertions_test() {
+    //clang-format off
+    static_assert(
+        psg::is_same_v<int, int> &&
+        psg::is_same_v<double, double> &&
+        psg::is_same_v<void, void> &&
+        psg::is_same_v<float, float>
+    );
+
+    static_assert(psg::true_type{});
+    static_assert(!psg::false_type{});
+
+    static_assert(psg::is_same_v<
+                    int,
+                    psg::remove_reference_t<int&>
+    >);
+
+    static_assert(psg::is_same_v<
+                    int,
+                    psg::remove_reference_t<int&&>
+    >);
+
+    static_assert(psg::is_same_v<
+                    const volatile int,
+                    psg::remove_reference_t<const volatile int&>
+    >);
+
+    static_assert(psg::is_same_v<
+                    int&,
+                    psg::add_lvalue_reference_t<int>
+    >);
+
+    static_assert(psg::is_same_v<
+                    int&,
+                    psg::add_lvalue_reference_t<int&>
+    >);
+
+    static_assert(psg::is_same_v<
+                    int&&,
+                    psg::add_rvalue_reference_t<int&&>
+    >);
+}
