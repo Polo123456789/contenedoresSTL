a3d112cad0cff08fe45b7f67636a2f7870fdead5
polo123456789 <pabsan02@gmail.com>
2020-12-27 16:21:55 -0600

    (+)<unique_ptr> Modifiers

--- include/psg/memory/unique_ptr/single_modifiers.hpp
+++ include/psg/memory/unique_ptr/single_modifiers.hpp
@@ -7,6 +7,27 @@
 
 namespace psg {
 
+template<class T, class Deleter>
+auto unique_ptr<T, Deleter>::release() noexcept -> pointer {
+    pointer old_obj = object;
+    object = nullptr;
+    return old_obj;
+}
+
+template<class T, class Deleter>
+void unique_ptr<T, Deleter>::swap(unique_ptr &u) noexcept {
+    psg::swap(u.object, object);
+    psg::swap(u.deleter, deleter);
+}
+
+template<class T, class Deleter>
+void unique_ptr<T, Deleter>::reset(pointer p) noexcept {
+    if (object != nullptr) {
+        deleter(object);
+    }
+    object = p;
+}
+
 } // namespace psg
 
 #endif
