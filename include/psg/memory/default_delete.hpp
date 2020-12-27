14767d0e6760afd972ac7a6bd1a9eb9c308d3b9b
polo123456789 <pabsan02@gmail.com>
2020-12-27 16:49:39 -0600

    (+)<default_delete> Permite trabajar con clases derivadas

--- include/psg/memory/default_delete.hpp
+++ include/psg/memory/default_delete.hpp
@@ -9,15 +9,31 @@ namespace psg {
 /// El delete default que se usa para el unique_ptr.
 template<typename T>
 struct default_delete {
-    void operator()(pgsl::owner<T*> p) noexcept {
+    void operator()(pgsl::owner<T *> p) noexcept {
         delete p; // NOLINT
+    }
+    template<typename U>
+    void operator()(pgsl::owner<U *> p) noexcept {
+        delete p; // NOLINT
+    }
+    template<typename U>
+    default_delete &operator=(const default_delete<U> & /*unused*/) {
+        return *this;
     }
 };
 
 template<typename T>
-struct default_delete<T[]> {
-    void operator()(pgsl::owner<T*> p) noexcept {
+struct default_delete<T[]> { // NOLINT
+    void operator()(pgsl::owner<T *> p) noexcept {
         delete[] p; // NOLINT
+    }
+    template<typename U>
+    void operator()(pgsl::owner<U *> p) noexcept {
+        delete[] p; // NOLINT
+    }
+    template<typename U>
+    default_delete &operator=(const default_delete<U[]> & /*unused*/) {//NOLINT
+        return *this;
     }
 };
 
