fa34f624e5692aad7f6d4302087e405067bca76d
polo123456789 <pabsan02@gmail.com>
2021-05-24 15:37:44 -0600

    (T)<tests> Manual test renamed to .cxx
    Travis compiles all .cpp files for tests, so we got 2 mains. So the ones
    I use for manual testing will be using the cxx extension

--- tests/scripts/quick_compile.bat
+++ tests/scripts/quick_compile.bat
@@ -1,1 +1,1 @@
-clang vector.cpp -I../include -Wall -Werror -Wextra -Wpedantic -std=c++17 -fsanitize=address -O3
+clang vector.cxx -I../include -Wall -Werror -Wextra -Wpedantic -std=c++17 -fsanitize=address -O3
