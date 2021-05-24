8359cc3c14d56775e606d0f7e070d2936bbe9f2a
polo123456789 <pabsan02@gmail.com>
2021-05-24 11:49:58 -0600

    (-)<pgsl/owners> Removed circular dependecy to vector

--- include/pgsl/owners.hpp
+++ include/pgsl/owners.hpp
@@ -3,7 +3,6 @@
 
 #include <psg/memory.hpp>
 #include <psg/array.hpp>
-#include <psg/vector.hpp>
 
 namespace pgsl {
 
@@ -12,9 +11,6 @@ using psg::unique_ptr;
 
 template<typename T, size_t size>
 using stack_array = psg::array<T, size>;
-
-template<typename T, typename Alloc>
-using dyn_array = psg::vector<T, Alloc>;
 
 } // namespace pgsl
 
