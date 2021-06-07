3b117b1a8c0f45fafe29ba8a0c005aa0e212799b
polo123456789 <pabsan02@gmail.com>
2021-06-06 18:27:38 -0600

    (+)<pgsl/utilities> Documentacion

--- include/pgsl/utilities.hpp
+++ include/pgsl/utilities.hpp
@@ -6,18 +6,36 @@
 
 namespace pgsl {
 
+/**
+ * Tag para las variables que se usan para indexar
+ */
 using index = size_t;
 
+/**
+ * Un static_cast que declara la intencion de que se haga el cast a un tipo de
+ * menor tamaño.
+ */
 template<typename T, typename Input>
 constexpr T narrow_cast(Input x) {
     return static_cast<T>(x);
 }
 
+/**
+ * La clase que tira pgsl::narrow si la conversion fallo
+ */
 class narrowing_error : public psg::exception {
    public:
-    explicit narrowing_error(const char *message) : psg::exception(message) {}
+    inline explicit narrowing_error(const char *message)
+        : psg::exception(message) {}
 };
 
+/**
+ * Un static_cast que declara la intencion de que se haga el cast a un tipo de
+ * menor tamaño.
+ *
+ * @throws pgsl::narrowing_error En caso de que el cast resulta en perdida de
+ *                               datos
+ */
 template<typename T, typename Input>
 T narrow(Input x) {
     T narrowed = static_cast<T>(x);
@@ -28,11 +46,14 @@ T narrow(Input x) {
     return narrowed;
 }
 
+/**
+ * Clase que en su destructor ejecuta la accion que se le dio.
+ */
 template<typename FinalAction>
 class final_action { // NOLINT No los necesita
    public:
     constexpr explicit final_action(FinalAction f) : action(f) {}
-    ~final_action() {
+    ~final_action() noexcept {
         action();
     }
 
@@ -40,6 +61,20 @@ class final_action { // NOLINT No los necesita
     FinalAction action;
 };
 
+/**
+ * Utilidad para crear acciones finales.
+ *
+ * **Nota:** Los destructores son `noexcept`, asi que el lambda no tiene que
+ * tirar excepciones
+ *
+ * Uso:
+ *
+ * ~~~
+ * auto clean_up = pgsl::finally([&]() noexcept {
+ *      // Clean up code
+ * });
+ * ~~~
+ */
 template<typename FinalAction>
 constexpr final_action<FinalAction> finally(FinalAction f) {
     return final_action(f);
