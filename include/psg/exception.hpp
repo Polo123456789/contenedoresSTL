079d671d4d12a48adc6ad4b7bba75de94cb8155d
polo123456789 <pabsan02@gmail.com>
2021-06-06 19:09:19 -0600

    (+)<exception> Documentacion

--- include/psg/exception.hpp
+++ include/psg/exception.hpp
@@ -1,23 +1,3 @@
-/*
- * .------..------..------..------..------.
- * |P.--. ||A.--. ||B.--. ||L.--. ||O.--. |
- * | :/\: || (\/) || :(): || :/\: || :/\: |
- * | (__) || :\/: || ()() || (__) || :\/: |
- * | '--'P|| '--'A|| '--'B|| '--'L|| '--'O|
- * `------'`------'`------'`------'`------'
- * .------..------..------..------..------..------..------.
- * |S.--. ||A.--. ||N.--. ||C.--. ||H.--. ||E.--. ||Z.--. |
- * | :/\: || (\/) || :(): || :/\: || :/\: || (\/) || :(): |
- * | :\/: || :\/: || ()() || :\/: || (__) || :\/: || ()() |
- * | '--'S|| '--'A|| '--'N|| '--'C|| '--'H|| '--'E|| '--'Z|
- * `------'`------'`------'`------'`------'`------'`------'
- *
- * Copyright (c) 2020 Pablo Sanchez Galdamez
- *
- * Mantengamos las excepciones sencillas. Para que tener toda una jerarquia
- * cuando una basta
- * */
-
 #ifndef PSG_EXCEPTION_HPP
 #define PSG_EXCEPTION_HPP
 
@@ -25,12 +5,17 @@
 
 namespace psg {
 
+/**
+ * Clase encargada de las excepciones en la libreira
+ */
 class exception : public std::exception {
     const char *message = "psg::exception";
 
    public:
     exception() = default;
-    explicit exception(const char *message) : message(message) {}
+
+    inline explicit exception(const char *message) : message(message) {}
+
     [[nodiscard]] const char *what(void) const noexcept override {
         return message;
     }
